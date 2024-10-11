66795ce1be7d9d0bae004a7853c9c07b214e2cba
DanniPilla <daniellapilla08@gmail.com>
2024-10-12 10:49:07 +1100

    Established some base HTML structure with template, CSS ready for customisation, and js for search input and filtering. Need further work to hook with backend API

---
+++ fashion.js
@@ -0,0 +1,52 @@
+
+let subcultures=[];
+
+function fetchPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        .then(response => response.json())
+        .then(posts => {
+            subcultures=posts;
+            displaySubcultures(subcultures);
+        });
+    }
+
+    function displaySubcultures(subcultures){
+            //  the second time you use .then(), you are defining the name of the variable that will hold the array of objects from the JSON response. You can give it any name, and it will represent the data returned from the API.
+            const postList = document.getElementById('post-list');
+            const template = document.getElementById('post-template');
+            postList.innerHTML = ''; // Clear previous posts
+
+            // Loop through posts and use the template to create cards
+            posts.forEach(post => {
+                const clone = template.content.cloneNode(true);
+                clone.querySelector('.subculture-title').innerText = post.title;
+                clone.querySelector('.subculture-overview').innerText = post.body;
+                clone.querySelector('.origins-content').innerText = post.body;
+                clone.querySelector('.style-basics-content').innerText = post.body;
+                clone.querySelector('.gallery-content').innerText = post.body;
+                postList.appendChild(clone);
+            });
+}
+
+function searchItems() {
+
+    const searchInput = document.getElementById('search-input').value.toLowerCase();
+    const filteredSubcultures = subcultures.filter(subcultures =>
+        subculture.title.toLowerCase().includes(searchInput) || //Boolean, so if the search input matches it will display in category
+        subculture.category.toLowerCase().includes(searchInput)
+    );
+
+    displayPosts(filteredSubcultures);
+}
+
+function filterByCategory(category) {
+
+    if (category === 'all') {
+        displaySubcultures(subcultures);
+    } else {
+        const filteredSubcultures = subcultures.filter(subcultures => subculture.category.toLowerCase() === subculture.toLowerCase());
+        displaySubcultures(filteredSubcultures);
+    }
+}
+
+fetchPosts();
